2b2b8f3963184cacfe7e01c38bb24f96a9f1055f
DarioSindicic <dario.sindicic@gmail.com>
2019-12-04 08:25:03 +0100

    day3

---
+++ day3/day3-b.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -eo pipefail
+
+IFS=','
+
+read -r -a array <<< `cat input.in | head -n1`
+read -r -a array2 <<< `cat input.in | head -n2 | tail -n1`
+
+dist=100000000
+posx=0
+posy=0
+declare -A mat1
+declare -A mat2
+cnt2=0
+for elem in "${array[@]}"
+do
+    smjer=${elem:0:1}
+    broj=${elem:1:100}
+    dx=0
+    dy=0
+    if [ $smjer = "R" ]; then
+        dy=1
+    elif [ $smjer = "L" ]; then
+        #posx=$($pos)
+        dy=-1
+    elif [ $smjer = "U" ]; then
+        dx=-1
+    elif [ $smjer = "D" ]; then
+        dx=1
+    else 
+        echo "ERROR"
+        exit 1
+    fi
+    for ((i=0; i < $broj; ++i)) 
+    do
+        posx=$(($posx+$dx))
+        posy=$(($posy+$dy))
+        cnt2=$(($cnt2+1))
+        if [ -v ${mat1[$posx,$posy]} ]; then
+            mat1[$posx,$posy]=$cnt2;
+        fi
+    done
+done
+posx=0
+posy=0
+cnt=0
+for elem in "${array2[@]}"
+do
+
+    smjer=${elem:0:1}
+    broj=${elem:1:100}
+    dx=0
+    dy=0
+    if [ $smjer = "R" ]; then
+        dy=1
+    elif [ $smjer = "L" ]; then
+        dy=-1
+    elif [ $smjer = "U" ]; then
+        dx=-1
+    elif [ $smjer = "D" ]; then
+        dx=1
+    else 
+        echo "ERROR"
+        exit 1
+    fi
+    for (( i=0; i < $broj; ++i )) 
+    do
+        posx=$(($posx+$dx))
+        posy=$(($posy+$dy))
+        cnt=$((cnt+1))
+        #printf "%s. (%s,%s)\n" $i $posx $posy
+        if ! [ -v ${mat1[$posx,$posy]} ]; then
+            echo "Found"
+            echo $(($cnt+${mat1[$posx,$posy]}))
+            newX=$posx
+            newY=$posy
+            if [ $posx -lt 0 ]; then
+                newX=$((-$posx))
+            fi
+            if [ $posy -lt 0 ]; then
+                newY=$((-$posy))
+            fi
+            newD=$(($newX+$newY))
+            if [ $dist -ge $newD ]; then
+                dist=$newD
+            fi
+        #else 
+            #echo "not found"
+        fi
+    done
+done
+echo $dist
